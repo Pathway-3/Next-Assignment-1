9ebebd6a0e787ad34f9e10992838c4a8ecf93f36
jordan toujouse <jtoujo1@gmail.com>
2024-04-07 12:47:36 -0500

    edited info on my main page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import Header from "./componets/Header";
 import listOfNames from "./componets/list of name";
-import ListPrompt from "inquirer/lib/prompts/list";
 
 listOfNames;
 export default function Home() {
