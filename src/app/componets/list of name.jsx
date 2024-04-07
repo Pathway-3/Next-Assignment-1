e3f82f77ac4aea340f0449e9213cdb6abd218467
jordan toujouse <jtoujo1@gmail.com>
2024-04-07 12:46:39 -0500

    added content for my pages.

---
+++ src/app/componets/list of name.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function listOfNames() {
+  return (
+    <div className="flex">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Store</li>
+      </ul>
+    </div>
+  );
+}
